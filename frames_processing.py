6c1f92ac417bcac85f8faf9c294a8fcb538781e1
pablodawson <pablodawson2@gmail.com>
2024-03-30 17:06:35 -0300

    separate video and frames processing

--- frames_processing.py
+++ frames_processing.py
@@ -4,15 +4,14 @@ from PIL import Image
 from marigold.marigold_pipeline import MarigoldPipeline
 from flow_estimation import FlowEstimator
 import torch
-import cv2
+import glob
 from utils import warp_with_flow, ghosting_mask, inpaint_depth
 import tqdm
 import os
 import time
 
 parser = argparse.ArgumentParser(description='Extract depth from video frames.')
-parser.add_argument('--video_path', type=str, help='Path to the directory containing input frames')
-parser.add_argument('--duration', type=int, default=100, help='Duration in frames to be processed')
+parser.add_argument('--frames_path', type=str, help='Path to the directory containing input frames')
 parser.add_argument('--noise_ratio', type=float, default=0.65, help='How much to base the prediction on the previous depth map')
 parser.add_argument('--blend_ratio', type=float, default=0.2, help='How much to blend in the previous depth map')
 
@@ -20,7 +19,7 @@ parser.add_argument('--remove_ghosting', type=bool, default=True, help='Detect a
 parser.add_argument('--moved_ratio_threshold', type=float, default=0.01, help='Threshold for movement detection')
 parser.add_argument('--depth_diff_threshold', type=float, default=0.01, help='Threshold for depth difference detection')
 
-parser.add_argument('--output_path', type=str, default='depth.mp4', help='Path to save the output frames')
+parser.add_argument('--output_path', type=str, default='video_out_standard', help='Path to save the output frames')
 parser.add_argument('--debug', type=bool, default=True)
 
 args = parser.parse_args()
@@ -35,21 +34,12 @@ flow_estimator = FlowEstimator("gmflow/pretrained/gmflow_things-e9887eda.pth", "
 
 os.makedirs(args.output_path, exist_ok=True)
 
-cap = cv2.VideoCapture(args.video_path)
-ret,frame = cap.read()
-frame_height, frame_width, _ = frame.shape
-out = cv2.VideoWriter(args.output_path ,cv2.VideoWriter_fourcc('X','V','I','D'), 10, (frame_width,frame_height))
+images = glob.glob(args.frames_path + "/*.png")
+images.sort()
 
-prev_image = None
-idx = 0
-
-while cap.isOpened():
-    timestart = time.time()
-    ret,frame = cap.read()
-    if not ret:
-        break
-
-    cur_image = Image.fromarray(frame)
+for i, image_path in tqdm.tqdm(enumerate(images), total=len(images)):
+    cur_image = Image.open(image_path)
+    prev_image = Image.open(images[i - 1]) if i > 0 else None
 
     if prev_image is not None:
         flow = flow_estimator.estimate_flow(np.array(prev_image), np.array(cur_image))
@@ -58,16 +48,20 @@ while cap.isOpened():
         if args.remove_ghosting:
             mask = ghosting_mask(flow, warped_depth, prev_depth, 
                                  moved_ratio_threshold=args.moved_ratio_threshold, depth_diff_threshold=args.depth_diff_threshold,
-                                 debug=args.debug, idx=idx)
+                                 debug=args.debug, idx=i)
 
             timestart = time.time()
             
             d_in = Image.fromarray(inpaint_depth(warped_depth, mask))
+
+            if args.debug:
+                print(f"Inpainting: {time.time() - timestart}")
+
         else:
             d_in = Image.fromarray(warped_depth * 255)
 
-    if idx==0:
-        pipeline_output = pipe(cur_image, input_depth=None, denoising_steps=1, ensemble_size=5, show_progress_bar=False)
+    if True:
+        pipeline_output = pipe(cur_image, input_depth=None, denoising_steps=1, ensemble_size=5)
         depth = pipeline_output.depth_np
         prev_depth = depth
     else:
@@ -76,15 +70,4 @@ while cap.isOpened():
                                show_progress_bar=False)
         prev_depth = pipeline_output.depth_np
     
-    idx += 1
-    prev_image = cur_image
-
-    if idx > args.duration:
-        break
-
-    print(f"Frame {idx} processed in {time.time() - timestart} seconds")
-    
-    out.write(cv2.cvtColor(np.array(pipeline_output.depth_colored), cv2.COLOR_RGB2BGR))
-
-cap.release()
-out.release()
+    pipeline_output.depth_colored.save(os.path.join(args.output_path, f"{str(i).zfill(4)}.png"))
